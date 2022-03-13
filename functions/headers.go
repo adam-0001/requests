7e6aba8bc3ca281cad29c6fee211219a111d32d8
Adam K <akandlk@outlook.com>
2022-03-12 23:47:47 -0800

    update default headers

--- functions/headers.go
+++ functions/headers.go
@@ -86,7 +86,7 @@ func FillNeededHeaders(host string, headers *http.Header) {
 
 func defaultHeaders(host string) map[string][]string {
 	return (map[string][]string{
-		"User-Agent":      {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"},
+		"User-Agent":      {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"},
 		"Accept":          {"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"},
 		"Accept-Encoding": {"gzip, deflate, br, utf-8"},
 		"Accept-Language": {"en-US,en;q=0.9"},
