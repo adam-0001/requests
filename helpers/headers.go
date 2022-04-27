9cc3b79adf401f8ab1a7264723be93bfbf9ea2f7
Adam <75471379+adam-0001@users.noreply.github.com>
2022-04-27 15:18:01 -0700

    nada

--- helpers/headers.go
+++ helpers/headers.go
@@ -22,13 +22,12 @@ func MakeHeaders(headers []map[string]string) map[string][]string {
 	return reqHeaders
 }
 
-func contains(s []string, str string) bool {
+func contains[A compareable](s []A, str A) bool {
 	for _, v := range s {
 		if v == str {
 			return true
 		}
 	}
-
 	return false
 }
 
@@ -50,7 +49,7 @@ func InferContentType(contentType string, headers *map[string][]string) {
 
 func FillNeededHeaders(host string, headers *http.Header) {
 	defHeaders := defaultHeaders(host)
-	keysLower := make([]string, len(*headers))
+	keysLower := [len(*headers)]string
 	i := 0
 	for k := range *headers {
 		keysLower[i] = strings.ToLower(k)
@@ -69,7 +68,7 @@ func defaultHeaders(host string) map[string][]string {
 	return (map[string][]string{
 		"User-Agent":      {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"},
 		"Accept":          {"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"},
-		"Accept-Encoding": {"gzip, deflate, br, utf-8"},
+		"Accept-Encoding": {"gzip, deflate, br"},
 		"Accept-Language": {"en-US,en;q=0.9"},
 		"Connection":      {"keep-alive"},
 		"Host":            {host},
